3db1502d7a1f5400491fc0cebedfbce0107ff0bd
ofeliapnr <ofeliapnr@gmail.com>
2026-04-26 20:48:20 +0000

    trying something different with dropdown menus

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+/*function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
@@ -18,4 +18,24 @@ function myFunction() {
         }
       }
     }
-  }
+  }*/
+ // Source - https://stackoverflow.com/a/68464106
+// Posted by Kinglish
+// Retrieved 2026-04-26, License - CC BY-SA 4.0
+
+//trying smth new
+
+window.addEventListener('load', function() {
+  // wait until the page loads before working with HTML elements
+  document.addEventListener('click', function(event) {
+    //click listener on the document
+    document.querySelectorAll('.dropdown-content').forEach(function(el) {
+      if (el !== event.target) el.classList.remove('show')
+      // close any showing dropdown that isn't the one just clicked
+    });
+    if (event.target.matches('.dropbtn')) {
+      event.target.closest('.dropdown').querySelector('.dropdown-content').classList.toggle('show')
+    }
+    // if this is a dropdown button being clicked, toggle the show class
+  })
+})
